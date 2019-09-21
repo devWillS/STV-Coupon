2227e4b6b6ae6e8e08afa879e7e476db7780e4cd
Seidi Nakamura <seidi.nakamura@st-ventures.jp>
2019-09-21 10:17:09 +0900

    create project

---
+++ STV-Coupon/STV-Coupon/Controllers/CoupnList/CouponListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CouponListViewController.swift
+//  STV-Coupon
+//
+//  Created by devWill on 2019/09/21.
+//  Copyright © 2019 devWill. All rights reserved.
+//
+
+import UIKit
+
+class CouponListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
